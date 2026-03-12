efb382ebe32955fb8b65b2434ce39f38517c02ed
Achilike <achilikes@gmail.com>
2026-03-12 19:59:14 +0100

    New Project

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Exit");
+
+            Console.Write("What would you like to do? ");
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = prompt;
+                entry._entryText = response;
+
+                journal.AddEntry(entry);
+            }
+
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+
+            else if (choice == 3)
+            {
+                Console.Write("Enter filename: ");
+                string filename = Console.ReadLine();
+
+                journal.SaveToFile(filename);
+            }
+
+            else if (choice == 4)
+            {
+                Console.Write("Enter filename: ");
+                string filename = Console.ReadLine();
+
+                journal.LoadFromFile(filename);
+            }
+        }
     }
 }
